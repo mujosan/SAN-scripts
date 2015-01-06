9e6c3715a3b1d16877a6e0d8a208395c5016a50c
Martin Horner <martin@mujosan.com>
2015-01-06 09:25:48 +0000

    Updated copyright header

--- Cisco/cisco_show_version.rb
+++ Cisco/cisco_show_version.rb
@@ -2,7 +2,7 @@
 #
 
 #--
-# Copyright 2012 by Martin Horner (martin.horner@telecom.co.nz)
+# Copyright 2014 by Martin Horner (martin@mujosan.com)
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to
