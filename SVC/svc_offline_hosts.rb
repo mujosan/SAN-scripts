a61a302de090c3ab3d56c45c91c6d985b80fb5e9
Martin Horner <martin@mujosan.com>
2015-02-13 08:51:51 +0000

    Using env in shebang line

--- SVC/svc_offline_hosts.rb
+++ SVC/svc_offline_hosts.rb
@@ -1,7 +1,7 @@
-#!/usr/local/bin/ruby
+#!/usr/bin/env ruby
 
 #--
-# Copyright 2014 by Martin Horner (martin@mujosan.com)
+# Copyright 2015 by Martin Horner (martin@mujosan.com)
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to
