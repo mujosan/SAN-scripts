285d4c402eee4e83385cbb3573818bb14e9b030d
Martin Horner <martin@mujosan.com>
2015-02-20 08:48:13 +0000

    Generic switch names. Generic SID. Removed redundant comment

--- Symmetrix/VmaxFriendlyWWNs.rb
+++ Symmetrix/VmaxFriendlyWWNs.rb
@@ -13,9 +13,9 @@ require 'rubygems'
 require 'net/ssh'
 require 'yaml'
 
-switches = ['cisdox01','cisdox02','cisdox07','cisdox08','cisdox09','cisdox10','ciswas01','ciswas02','ciswas07','ciswas08','ciswas09','ciswas10','ciswyn07','ciswyn08','ciswyn09','ciswyn10','ciswyn11','ciswyn12']
+switches = ['switch01','switch02','switch03','switch04']
 
-frames = ['620']
+frames = ['123']                                                          # VMAX SID
 
 puts "Switches"
 puts "========"
@@ -28,7 +28,6 @@ switches.each do |name|
   Net::SSH.start( name, "script", :password => 'password' ) do |ssh|
     result = ssh.exec!("show running-config")
     @fcalias = result.delete(":").scan(/fcalias name (\w+) (\w+) (\w+)\n\s+(\w+) (\w+) (\w+)/)
-#    @fcalias = result.delete(":").scan(/fcalias name (\w+-\w+) (\w+) (\w+)\n\s+(\w+) (\w+) (\w+)/) # Telecom specific HBA naming
     @fcalias.each do |host|
       @aliaspwwn[host[0]] = host[5] if host[0] =~ /\w+_[A|B]/
     end
@@ -46,8 +45,8 @@ frames.each do |sid|
   symaccess = %x[symaccess -sid #{sid} list logins]
   symaccess.each("\n") do |line|
     case line
-    when /Fibre/                                       # when line contains "Fibre"
-    # If the identifier is equal the node name (i.e. friendly name not set) AND the HBA is logged in to the FA
+    when /Fibre/
+    # If the identifier is equal to the node name (i.e. friendly name not set) AND the HBA is logged in to the FA
       if (line.split[0] == line.split[2]) && (line.split[5] == 'Yes')
         if @aliaspwwn.has_value?(line.split[0])      # If the hash of host WWNs from the switches has a match
           friendlyname = @aliaspwwn.index(line.split[0]).gsub(/_/, '/')
